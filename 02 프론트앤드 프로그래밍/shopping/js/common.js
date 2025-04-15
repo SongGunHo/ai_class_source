8c340cd18390ff2973d331ccc46534c975c92615
SongGunHo <jigu1993@naver.com>
2025-04-15 16:59:05 +0900

    items.js저장

---
+++ 02 프론트앤드 프로그래밍/shopping/js/common.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded",function(){
+
+    new swiper(".mySwiper", {
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+        pagination: {
+            el: ".swiper-pagination",
+            
+        };
+)}; };
+ 
