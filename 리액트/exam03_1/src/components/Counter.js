9e54e14401e9ee4592679459df3cc83a906e5864
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-12 18:22:09 +0900

    확인 하자

--- 리액트/exam03_1/src/components/Counter.js
+++ 리액트/exam03_1/src/components/Counter.js
@@ -2,12 +2,19 @@ import { useState } from "react";
 
 const Counter =() =>{
     const [number, setNumber] = useState(0);
+    let cnt = 0;
     console.log('호출', number)
+    
+    const onPlus = () =>{
+        setNumber(number +1)
+        console.log('cnt', cnt)
+        cnt ++;
+    }
     return (
         <>
             <h1>{number}</h1>
             <button type="button" onClick={()=> setNumber(number -1)}>-1</button>
-            <button type="button" onClick={() => setNumber(number +1)}>+1</button>
+            <button type="button" onClick={onPlus}>+1</button>
         </>
     )
 };
